eba912ffc0c044335755f0056d4e28d71bad5892
iris <iriskurien@gmail.com>
2025-03-22 21:50:55 +0530

    add animation

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 import "./LandingPage.css";
 
-function LandingPage(){
-        return (
+function LandingPage() {
+    const textRef = useRef(null);
+
+    useEffect(() => {
+        const words = textRef.current.querySelectorAll("span");
+        
+        gsap.fromTo(
+            words, 
+            { opacity: 0, y: 30 }, 
+            { opacity: 1, y: 0, duration: 1, stagger: 0.2, ease: "power2.out" }
+        );
+    }, []);
+
+    return (
         <div className="landing-page">
-            <h1>improve <span className="font-luxury">your</span> <br/> skills 
-            <span className="font-luxury">with</span>
-            <span className="highlight">saturday <br/> hacknight</span>
-     </h1>
+            <h1 ref={textRef}>
+                <span>improve</span> <span className="font-luxury">your</span> <br />
+                <span className="no-break">skills <span className="font-luxury">with</span> <span className="highlight">saturday</span></span> <br />
+                <span className="highlight">hacknight</span>
+            </h1>
         </div>
     );
 }
