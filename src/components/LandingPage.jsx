138b8f5767472095b506190f3121033bf3d8f043
iris <iriskurien@gmail.com>
2025-03-22 21:27:55 +0530

    setup landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./LandingPage.css";
+
+function LandingPage(){
+        return (
+        <div className="landing-page">
+            <h1>improve <span className="font-luxury">your</span> <br/> skills 
+            <span className="font-luxury">with</span>
+            <span className="highlight">saturday <br/> hacknight</span>
+     </h1>
+        </div>
+    );
+}
+
+export default LandingPage;
